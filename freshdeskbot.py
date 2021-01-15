1152c37c12f30a0f1880ed607ff6b9256d0bffa0
Dave Arter <davea@mysociety.org>
2021-01-15 21:13:52 +0000

    Post summary to Slack

--- freshdeskbot.py
+++ freshdeskbot.py
@@ -2,7 +2,6 @@
 
 import json
 import os
-from pprint import pprint
 import requests
 import humanize
 from operator import itemgetter
@@ -32,11 +31,25 @@ def call_freshdesk_api(url, add_root=True, **kwargs):
     return results
 
 
+def call_slack_webhook(payload):
+    requests.post(SLACK_URL, data={"payload": json.dumps(payload)})
+
+
 def format_reply_time(time):
     return humanize.naturaltime(
         datetime.utcnow() - datetime.strptime(time, "%Y-%m-%dT%H:%M:%SZ")
     )
 
+
+slack_message = {
+    "channel": SLACK_CHANNEL,
+    "username": "Freshdesk summary",
+    "icon_emoji": ":freshdesk:",
+    "text": "*<https://{}.freshdesk.com/a/tickets/filters/search?orderBy=updated_at&orderType=desc&q[]=agent%3A%5B0%5D&q[]=status%3A%5B0%5D&ref=all_tickets|Your open Freshdesk tickets>*".format(
+        FRESHDESK_URL
+    ),
+    "attachments": [],
+}
 
 tickets = call_freshdesk_api("tickets", filter="new_and_my_open")
 for ticket in sorted(tickets, key=itemgetter("updated_at"), reverse=True):
@@ -45,14 +58,30 @@ for ticket in sorted(tickets, key=itemgetter("updated_at"), reverse=True):
     msgs = [m for m in conversations if not m["private"]]
     customer_replied = msgs[-1]["incoming"] if msgs else None
     last_reply = format_reply_time(msgs[-1]["created_at"]) if msgs else None
-    company_name = ticket["company"]["name"]
+    company = ticket["company"]["name"]
     reply_desc = (
         "{} replied {}".format("Customer" if customer_replied else "Agent", last_reply)
         if last_reply
         else ""
     )
-    print(
-        "#{id} - {company_name} - {subject} {reply_desc}".format(
-            reply_desc=reply_desc, company_name=company_name, **ticket
-        )
+    fallback = "#{id} - {company_name} - {subject} {reply_desc}".format(
+        reply_desc=reply_desc, company_name=company, **ticket
     )
+    slack_message["attachments"].append(
+        {
+            "fallback": fallback,
+            "text": "<https://{url}.freshdesk.com/a/tickets/{id}|#{id}> {company}".format(
+                url=FRESHDESK_URL, id=ticket["id"], company=company
+            ),
+            "pretext": "",
+            "color": "#d00000" if customer_replied else "#00d000",
+            "fields": [
+                {
+                    "title": ticket["subject"],
+                    "value": reply_desc,
+                },
+            ],
+        }
+    )
+
+call_slack_webhook(slack_message)
