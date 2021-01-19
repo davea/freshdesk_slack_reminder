92cf9362aa2bf3908eda6917b9b276621b8c6546
Dave Arter <davea@mysociety.org>
2021-01-19 09:23:49 +0000

    Separate new & unassigned tickets, add summary to beginning of message.

--- freshdeskbot.py
+++ freshdeskbot.py
@@ -5,6 +5,7 @@ import os
 import requests
 import humanize
 from operator import itemgetter
+from itertools import groupby
 from datetime import datetime
 
 FRESHDESK_KEY = os.environ["FRESHDESK_KEY"]
@@ -45,43 +46,70 @@ slack_message = {
     "channel": SLACK_CHANNEL,
     "username": "Freshdesk summary",
     "icon_emoji": ":freshdesk:",
-    "text": "*<https://{}.freshdesk.com/a/tickets/filters/search?orderBy=updated_at&orderType=desc&q[]=agent%3A%5B0%5D&q[]=status%3A%5B0%5D&ref=all_tickets|Your open Freshdesk tickets>*".format(
-        FRESHDESK_URL
-    ),
     "attachments": [],
 }
 
 tickets = call_freshdesk_api("tickets", filter="new_and_my_open")
-for ticket in sorted(tickets, key=itemgetter("updated_at"), reverse=True):
-    ticket = call_freshdesk_api("tickets/{}".format(ticket["id"]), include="company")
-    conversations = call_freshdesk_api("tickets/{}/conversations".format(ticket["id"]))
-    msgs = [m for m in conversations if not m["private"]]
-    customer_replied = msgs[-1]["incoming"] if msgs else None
-    last_reply = format_reply_time(msgs[-1]["created_at"]) if msgs else None
-    company = ticket["company"]["name"]
-    reply_desc = (
-        "{} replied {}".format("Customer" if customer_replied else "Agent", last_reply)
-        if last_reply
-        else ""
-    )
-    fallback = "#{id} - {company_name} - {subject} {reply_desc}".format(
-        reply_desc=reply_desc, company_name=company, **ticket
-    )
-    slack_message["attachments"].append(
-        {
-            "fallback": fallback,
-            "text": "<https://{url}.freshdesk.com/a/tickets/{id}|#{id}> {company}".format(
-                url=FRESHDESK_URL, id=ticket["id"], company=company
-            ),
-            "pretext": "",
-            "color": "#d00000" if customer_replied else "#00d000",
-            "fields": [
-                {
-                    "title": ticket["subject"],
-                    "value": reply_desc,
-                },
-            ],
-        }
-    )
+awaiting_reply = 0
+for agent, tickets in groupby(
+    sorted(tickets, key=lambda t: t['responder_id'] or 0, reverse=True),
+    itemgetter("responder_id"),
+):
+    for i, ticket in enumerate(
+        sorted(tickets, key=itemgetter("updated_at"), reverse=True)
+    ):
+        pretext = ""
+        if i == 0:
+            if agent:
+                pretext = "*<https://{}.freshdesk.com/a/tickets/filters/search?orderBy=updated_at&orderType=desc&q[]=agent%3A%5B0%5D&q[]=status%3A%5B0%5D&ref=all_tickets|Your unresolved tickets>*".format(
+                    FRESHDESK_URL
+                )
+            else:
+                pretext = "*<https://{}.freshdesk.com/a/tickets/filters/search?orderBy=updated_at&orderType=desc&q[]=agent%3A%5B-1%5D&q[]=status%3A%5B0%5D&ref=all_tickets|New & unassigned tickets>*".format(
+                    FRESHDESK_URL
+                )
+
+        ticket = call_freshdesk_api(
+            "tickets/{}".format(ticket["id"]), include="company"
+        )
+        conversations = call_freshdesk_api(
+            "tickets/{}/conversations".format(ticket["id"])
+        )
+        msgs = [m for m in conversations if not m["private"]]
+        customer_replied = msgs[-1]["incoming"] if msgs else True # if no conversations assume it's a new incoming ticket
+        if customer_replied:
+            awaiting_reply += 1
+        last_reply = format_reply_time(msgs[-1]["created_at"]) if msgs else None
+        company = ticket["company"]["name"]
+        reply_desc = (
+            "{} replied {}".format(
+                "Customer" if customer_replied else "Agent", last_reply
+            )
+            if last_reply
+            else ""
+        )
+        fallback = "#{id} - {company_name} - {subject} {reply_desc}".format(
+            reply_desc=reply_desc, company_name=company, **ticket
+        )
+        slack_message["attachments"].append(
+            {
+                "fallback": fallback,
+                "text": "<https://{url}.freshdesk.com/a/tickets/{id}|#{id}> {company}".format(
+                    url=FRESHDESK_URL, id=ticket["id"], company=company
+                ),
+                "pretext": pretext,
+                "color": "#d00000" if customer_replied else "#00d000",
+                "fields": [
+                    {
+                        "title": ticket["subject"],
+                        "value": reply_desc,
+                    },
+                ],
+            }
+        )
+
+# Generate a nicer summary for e.g. notifications
+if awaiting_reply:
+    slack_message['text'] = "{} ticket{} awaiting reply".format(humanize.apnumber(awaiting_reply).title(), "s" if awaiting_reply > 1 else "")
 
 call_slack_webhook(slack_message)
